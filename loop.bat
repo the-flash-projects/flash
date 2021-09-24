5e91f6dc44563c09ceeed2aea5b1fbcf25647873
The_Flash <90428057+Savitar44@users.noreply.github.com>
2021-09-24 08:28:56 -0300

    Add files via upload

---
+++ loop.bat
@@ -0,0 +1,8 @@
+@echo off
+echo Concluído com Sucesso!
+tasklist | find /i "ngrok.exe" >Nul && goto check || echo "Não foi possível obter o túnel NGROK, certifique-se de que NGROK_AUTH_TOKEN está correto em Configurações> Segredos> Segredo do repositório. Talvez sua VM anterior ainda esteja em execução: https://dashboard.ngrok.com/status/tunnels" & ping 127.0.0.1 >Nul & exit
+:check
+ping 127.0.0.1 > null
+cls
+echo Concluído com Sucesso!
+goto check
